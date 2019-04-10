c50158de723a12c8083980ca5b05789549702028
Roman Dulman <47609139+romandulman@users.noreply.github.com>
2019-04-10 19:17:30 +0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 $(document).ready(function () {
+    $( "#allBtn" ).click(function() {
+        $.ajax({
+            url: "https://restcountries.eu/rest/v2/all",
+            type: "GET",
+            success: function (response) {
+                response.forEach(function (element) {
+                    $('#out').append(
+                        `        <div class="card col-md-6">
+            <div class="row no-gutters">
+                <div class="col-auto">
+                    <img src="${element.flag}" class="img-fluid" alt="">
+                </div>
+                <div class="col">
+                    <div class="card-block px-2">
+                        <h4 class="card-title" id="nameOut">${element.name}</h4>
+                        <p class="card-text" >Top Level Domain: <span id="domainOut">${element.topLevelDomain}</span></p>
+                        <p class="card-text" >Capital: <span id="capOut">${element.capital}</span></p>
+                        <p class="card-text" >Currencies: <span id="currOut">${element.currencies.code}</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    $.ajax({
-        url: "https://reqres.in/api/users?page=2",
-        type: "GET",
-        success: function (response) {
-            response.data.forEach(function (element) {
-                $('#out').append(
-                    `<h1>${element.last_name}</h1>`
-                )
-            });
-        }
+`
+                    )
+                });
+            }
+        });
     });
 
+    $( "#searchBtn" ).click(function() {
+
+    });
 });
